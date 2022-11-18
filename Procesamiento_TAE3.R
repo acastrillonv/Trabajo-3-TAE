8e3ed34ec4cf3e1fa4eec43570c9e9a25bb3d8a6
valevanec <vvanegas@unal.edu.co>
2022-11-18 11:31:58 -0500

    Selección de variables y reagrupamiento variable CLASE_ACCIDENTE

--- Procesamiento_TAE3.R
+++
@@ -1,7 +0,0 @@
-rm(list = ls())
- 
-library(tidyverse)
-library(lubridate) 
-library(dplyr)
-datos <- read.csv("incidentes_viales.csv", sep = ";", header = T)
-apply(is.na(datos),2,sum) # total de na's por variable

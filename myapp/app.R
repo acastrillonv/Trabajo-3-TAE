e189fdcd944a5f04ed7ba757a27646fd40a66adb
acastrillonv <48304499+acastrillonv@users.noreply.github.com>
2022-11-26 16:56:06 -0500

    configuraciones mapa

--- myapp/app.R
+++ myapp/app.R
@@ -1,34 +1,44 @@
 library(leaflet)
-library(RColorBrewer)
-library(scales)
-library(lattice)
 library(dplyr)
-library(fdm2id)
-library(flexclust)
+
 
 # See above for the definitions of ui and server
-ui <- fluidPage(tabPanel("Interactive map",
-                      div(class="outer",
-                          tags$head(
-                            # Include our custom CSS
-                            includeCSS("styles.css")
-                          ),
-                          
-                          # If not using custom CSS, set height of leafletOutput to a number instead of percent
-                          leafletOutput("map", width="100%", height="100%")
-                                        
-                          )
-                          
-                         
-                      )
-             )
+ui <- fluidPage(
+  titlePanel(p("Prediccion de incidentes viales en Medellin", style = "color:#3474A7")),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "TipoAccidente",
+        label = "Tipo de Accidente",
+        choices = c("Carro","Moto")
+      ),
+      selectInput(
+        inputId = "Mes",
+        label = "Mes",
+        choices = 1:12
+      ),
+      selectInput(
+        inputId = "Ano",
+        label = "Ano",
+        choices = 2014:2022
+      ),
+    ),
+    
+    
+    mainPanel(
+      leafletOutput(outputId = "map", width=1000, height=700),
+    )
+  )
+)
         
 
 server <- function(input, output) {
   
   # Create the map
   output$map <- renderLeaflet({
-    leaflet() %>% addProviderTiles("Stamen.TonerLite",options = providerTileOptions(noWrap = FALSE)) %>% setView(lng = -75.56, lat = 6.24, zoom = 12)
+    leaflet() %>% 
+    addProviderTiles("OpenStreetMap.Mapnik",options = providerTileOptions(noWrap = FALSE)) %>%
+    setView(lng = -75.56, lat = 6.24, zoom = 13)
     
   })
 }

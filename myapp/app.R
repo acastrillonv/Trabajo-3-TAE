3aeb25f0312372f873745c14cbdb60f34c2a02c1
acastrillonv <48304499+acastrillonv@users.noreply.github.com>
2022-11-26 23:00:50 -0500

    prueba

--- myapp/app.R
+++ myapp/app.R
@@ -22,33 +22,41 @@ library(dplyr)
 
 load("calculo_indice.RData")
 
+# sidebarLayout(
+#  sidebarPanel(
+#    selectInput(
+#      inputId = "TipoAccidente",
+#      label = "Tipo de Accidente",
+#      choices = c("Choque","Atropello","Caida de Ocupante","Volcamiento","Incendio","Otro")
+#    ),
+#    selectInput(
+#      inputId = "Mes",
+#      label = "Mes",
+#      choices = c("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre")
+#    ),
+#    selectInput(
+#      inputId = "Ano",
+#      label = "Ano",
+#      choices = 2014:2022
+#    ),
+#  )
+
 ui <- fluidPage(
-  titlePanel(p("Prediccion de incidentes viales en Medellin", style = "color:#3474A7")),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(
-        inputId = "TipoAccidente",
-        label = "Tipo de Accidente",
-        choices = c("Choque","Atropello","Caida de Ocupante","Volcamiento","Incendio","Otro")
-      ),
-      selectInput(
-        inputId = "Mes",
-        label = "Mes",
-        choices = c("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre")
-      ),
-      selectInput(
-        inputId = "Ano",
-        label = "Ano",
-        choices = 2014:2022
-      ),
+  navbarPage(
+    "Accidentalidad en Medellin",
+    tabPanel("Presentacion",
+             fluidRow(id='header1',
+                      column(12,
+                            ),
+                             
+                      )
+             ),
     ),
-    
-    
+  
     mainPanel(
       leafletOutput(outputId = "mapaBarrios", width=1000, height=700),
     )
   )
-)
         
 
 server <- function(input, output) {
@@ -56,7 +64,7 @@ server <- function(input, output) {
   barrios=readOGR("www/Limite_Barrio_Vereda_Catastral.shp",layer="Limite_Barrio_Vereda_Catastral")
   nombres_barrios=iconv(barrios@data$NOMBRE,"UTF-8","ISO_8859-1")
   
-  colorMaker <- colorFactor(palette = c("#0d6122", "#ffa333", "#d90000","#ff0000"), 
+  color <- colorFactor(palette = c("#0d6122", "#ffa333", "#d90000","#ff0000"), 
                             levels = c("Bajo", "Medio", "Alto", "Muy Alto"))
   pal <- colorFactor(
     palette = c("#0d6122", "#ffa333", "#d90000","#ff0000"),
@@ -69,7 +77,7 @@ server <- function(input, output) {
       addPolygons(data=barrios,
                   weight = 1,
                   color = "white",
-                  fillColor =colorMaker(calculo_indice$Riesgo),
+                  fillColor =color(calculo_indice$Riesgo),
                   fillOpacity = 0.8,
                   
                   label=paste0("<p style='font-size:20px'> <strong>Barrio: </strong>",calculo_indice$BARRIO,
